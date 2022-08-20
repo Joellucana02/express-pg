077612de3318886bf937f858648ec12e8fb2ff8a
joel <joellucana02@gmail.com>
2022-08-20 13:24:49 -0300

    added bcrypt to users route

--- node-bcrypt-sql/routes/users.js
+++ node-bcrypt-sql/routes/users.js
@@ -20,9 +20,36 @@ router.post("/", async (req, res, next) => {
     const hash = await bcrypt.hash(password, saltRounds);
     const user = await db.query(
       `INSERT INTO users (username,password) VALUES ($1,$2) RETURNING *`,
-      [req.body.name, hash]
+      [req.body.username, hash]
     );
     return res.json(user.rows[0]);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const passwordGiven = req.body.password;
+    const usernameGiven = req.body.username;
+
+    const user = await db.query(
+      `SELECT * FROM users WHERE username = ($1) LIMIT 1`,
+      [usernameGiven]
+    );
+    if (user.rows.length === 0) {
+      return res.json({ message: "User not found" });
+    }
+
+    const correctUser = await bcrypt.compare(
+      passwordGiven,
+      user.rows[0].password
+    );
+    if (correctUser === false) {
+      return res.json({ message: "Password does not match" });
+    }
+
+    return res.json({ message: "Logged in!" });
   } catch (error) {
     return next(error);
   }
